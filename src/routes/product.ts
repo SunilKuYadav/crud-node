59ab76e05156ce59c5d93c2d7dba220433ae53d5
Sunil Kumar Yadav <mobcoderid-105@Mobcoders-Mac-mini-2.local>
2022-04-08 17:02:34 +0530

    Update: router

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -13,9 +13,9 @@ router.get("/product/list", Product.index);
 router.post("/product/create", Product.create);
 
 // update a product
-router.patch("/product/update/:id", Product.update);
+router.patch("/product/update", Product.update);
 
 // delete a product
-router.delete("/product/delete/:id", Product.delete);
+router.delete("/product/delete", Product.delete);
 
 export default router;
