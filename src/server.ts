5295cd57ac0f7d541cd32e3fba10bc48ef328d83
Sunil Kumar Yadav <mobcoderid-105@Mobcoders-Mac-mini-2.local>
2022-04-08 17:03:31 +0530

    Feature: config swagger-ui

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
+
 import app from "./app";
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = 8000;
 
